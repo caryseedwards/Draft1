5a270629eb3fd9fec4aae8b6c6b602fd0dc8845c
caryseedwards <caryseedwards@gmail.com>
2023-11-05 19:26:47 +0000

    Sierpinski Shape class V1

--- src/main/java/patterns/SierpinskiShape.java
+++ src/main/java/patterns/SierpinskiShape.java
@@ -7,100 +7,108 @@ import shapes.Triangle;
 import javax.swing.*;
 import java.awt.*;
 
-import javax.swing.*;
-import java.awt.*;
-
-public class SierpinskiShape extends JFrame {
-    public static int ITERATIONS = 2;
-
-    public SierpinskiShape() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 800);
-        setVisible(true);
-    }
+public class SierpinskiShape extends JPanel {
+    enum Shape { TRIANGLE, CIRCLE, SQUARE, HEXAGON }
+    private Shape shapeToDraw = Shape.TRIANGLE;
+    private int depth = 5;
 
     @Override
-    public void paint(Graphics g) {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        drawSierpinskiTriangle(g2d, 400, 400, 200, ITERATIONS);
-       // drawSierpinskiCarpet(g2d, 400, 100, 50, ITERATIONS);
-       // drawSierpinskiHexagon(g2d, 100, 400, 50, ITERATIONS);
-       // drawApollonianGasket(g2d, 100, 100, 50, ITERATIONS);
+
+        switch (shapeToDraw) {
+            case TRIANGLE:
+                drawSierpinski(g2d, new Triangle(400, 1100, 350), depth);
+                break;
+            case CIRCLE:
+                drawGasket(g2d, 300, 300, 300, depth);
+                break;
+            case SQUARE:
+                drawCarpet(g2d, new Square(400, 400, 300), depth);
+                break;
+            case HEXAGON:
+                drawHexagon(g2d, getWidth() / 2, getHeight() / 2, 300, depth);
+                break;
+        }
     }
-
-
-        public void drawSierpinskiTriangle(Graphics2D g, int x, int y, int sideLength, int iterations) {
-            if (iterations == 0) return;
-
-            int height = (int) (Math.sqrt(3) * sideLength / 2);
-
-            // Draw the current triangle
-            int[] xPoints = {x, x - sideLength / 2, x + sideLength / 2};
-            int[] yPoints = {y, y + height, y + height};
-            g.setColor(Color.RED);
-            g.fillPolygon(xPoints, yPoints, 3);
-
-            // Calculate vertices of inner triangles
-            int x1 = x;
-            int y1 = y + height / 2;
-            int x2 = x - sideLength / 4;
-            int y2 = y + 3 * height / 4;
-            int x3 = x + sideLength / 4;
-            int y3 = y + 3 * height / 4;
-
-            // Recursively draw three smaller triangles
-            drawSierpinskiTriangle(g, x1, y1, sideLength / 2, iterations - 1);
-            drawSierpinskiTriangle(g, x2, y2, sideLength / 2, iterations - 1);
-            drawSierpinskiTriangle(g, x3, y3, sideLength / 2, iterations - 1);
+    public void drawSierpinski(Graphics2D g, Triangle triangle, int depth) {
+        if (depth == 0) {
+            triangle.draw(g, Color.BLACK, 1.0f, Color.BLACK, null);
+            return;
         }
 
+        double newRadius = triangle.radius / 2;
 
+        int midX1 = (triangle.x1 + triangle.x2) / 2;
+        int midY1 = (triangle.y1 + triangle.y2) / 2;
+        int midX3 = (triangle.x1 + triangle.x3) / 2;
+        int midY3 = (triangle.y1 + triangle.y3) / 2;
 
+        drawSierpinski(g, new Triangle(triangle.x1, triangle.y1 - (int) newRadius, newRadius), depth - 1);
+        drawSierpinski(g, new Triangle(midX1, midY1 - (int) newRadius, newRadius), depth - 1);
+        drawSierpinski(g, new Triangle(midX3, midY3 - (int) newRadius, newRadius), depth - 1);
+    }
+    private void drawGasket(Graphics g, int x, int y, int radius, int depth) {
+        if (depth <= 0) return;
 
+        g.drawOval(x - radius, y - radius, 2 * radius, 2 * radius);
 
+        int newRadius = radius / 2;
+        int dx = (int) (newRadius * Math.cos(Math.PI / 6));
+        int dy = (int) (newRadius * Math.sin(Math.PI / 6));
 
+        drawGasket(g, x, y - newRadius, newRadius, depth - 1);
+        drawGasket(g, x - dx, y + dy, newRadius, depth - 1);
+        drawGasket(g, x + dx, y + dy, newRadius, depth - 1);
+    }
+    public void drawHexagon(Graphics2D g, int x, int y, double radius, int depth) {
+        if (depth == 0) {
+            Hexagon hexagon = new Hexagon(x, y, radius);
+            hexagon.draw(g, Color.BLACK, 1.0f, Color.BLACK, "");
+            return;
+        }
 
-
-
-    private void drawSierpinskiCarpet(Graphics2D g, int x, int y, double radius, int iterations) {
-        if (iterations == 0) return;
-        Square square = new Square(x, y, radius);
-        square.draw(g, Color.BLACK, 1.0f, Color.BLUE, "solid");
         double newRadius = radius / 3;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) continue;
-                drawSierpinskiCarpet(g, x + (int) (i * newRadius * 2), y + (int) (j * newRadius * 2), newRadius, iterations - 1);
-            }
-        }
-    }
 
-    private void drawSierpinskiHexagon(Graphics2D g, int x, int y, double radius, int iterations) {
-        if (iterations == 0) return;
-        Hexagon hexagon = new Hexagon(x, y, radius);
-        hexagon.draw(g, Color.BLACK, 1.0f, Color.GREEN, "solid");
-        double newRadius = radius / 2;
-        drawSierpinskiHexagon(g, x, y, newRadius, iterations - 1);
+        // Draw 6 smaller hexagons around the central point
         for (int i = 0; i < 6; i++) {
-            int newX = (int) (x + newRadius * Math.cos(i * Math.PI / 3));
-            int newY = (int) (y + newRadius * Math.sin(i * Math.PI / 3));
-            drawSierpinskiHexagon(g, newX, newY, newRadius, iterations - 1);
+            int newX = x + (int) (newRadius * 2 * Math.cos(i * Math.PI / 3));
+            int newY = y + (int) (newRadius * 2 * Math.sin(i * Math.PI / 3));
+            drawHexagon(g, newX, newY, newRadius, depth - 1);
         }
-    }
 
-    private void drawApollonianGasket(Graphics2D g, int x, int y, double radius, int iterations) {
-        if (iterations == 0) return;
-        Circle circle = new Circle(x, y, (int) radius);
-        circle.draw(g, Color.BLACK, 1.0f, new Color(0, 0, 0, 0), "solid");
-        double newRadius = radius / 2;
-        for (int i = -1; i <= 1; i += 2) {
-            for (int j = -1; j <= 1; j += 2) {
-                drawApollonianGasket(g, x + (int) (i * newRadius), y + (int) (j * newRadius), newRadius, iterations - 1);
+        // Draw a smaller hexagon in the center
+        drawHexagon(g, x, y, newRadius, depth - 1);
+    }
+    public void drawCarpet(Graphics2D g, Square square, int depth) {
+        if (depth == 0) {
+            square.draw(g, Color.BLACK, 1.0f, Color.BLACK, null);
+            return;
+        }
+
+        double newRadius = square.radius / 3;
+        double offsetX = newRadius * 2;
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (row == 1 && col == 1) continue;
+
+                int newX = (int) (square.centerX + (col - 1) * offsetX);
+                int newY = (int) (square.centerY + (row - 1) * offsetX);
+
+                Square newSquare = new Square(newX, newY, newRadius);
+                drawCarpet(g, newSquare, depth - 1);
             }
         }
     }
 
     public static void main(String[] args) {
-        new SierpinskiShape();
+        JFrame frame = new JFrame("Sierpinski Shape");
+        SierpinskiShape sierpinskiShape = new SierpinskiShape();
+        frame.add(sierpinskiShape);
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
